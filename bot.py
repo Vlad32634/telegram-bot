94398942a2ed534d460461b4229e4ac8704d2ad2
Vlad32634 <studentinfizan1@gmail.com>
2025-03-24 19:36:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -162,11 +162,16 @@ async def start_handler(message: types.Message):
         subscribers.add(user_id)
         await notify_admins(f"➕ Новий підписник: {message.from_user.full_name} (@{message.from_user.username}, ID: {user_id})")
 
-    await bot.send_photo(message.chat.id, WELCOME_PHOTO_URL, caption="Привіт! Мене звати Влад, я масажист і реабілітолог 👨‍⚕️ з досвідом понад 5 років. В моєму телеграм боті ви можете:
-▪️ Отримати знижку на масаж! Тільки натиснути на кнопку: "Знижка"
-▪️ Підібрати для себе потрібний масаж, прочитавше все про них.
-▪️ Побачити актуальний прайс 
-▪️ Будете першими отримувати актуальні акційні пропозиції.")
+welcome_text = (
+        "Привіт! Мене звати Влад, я масажист і реабілітолог 👨‍⚕️ з досвідом понад 5 років.\n"
+        "В моєму телеграм-боті ви можете:\n"
+        "✔ Записатися на масаж\n"
+        "✔ Дізнатися про види масажу\n"
+        "✔ Переглянути прайс\n\n"
+        "Обирайте потрібний розділ нижче 👇"
+    )
+
+await bot.send_photo(message.chat.id, WELCOME_PHOTO_URL, caption=welcome_text, reply_markup=main_keyboard())
 
 # Функція для надсилання повідомлення адміністраторам
 async def notify_admins(text):
