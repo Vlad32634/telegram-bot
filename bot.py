998cc9a0ee3c6709be7c51b61dd6fd9bb08335be
Vlad32634 <studentinfizan1@gmail.com>
2025-03-28 11:15:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,6 +146,7 @@ PRICE_IMAGE_URL = "https://www.dropbox.com/scl/fi/z25kakyigrnuoz5idl1hv/photo_20
 def main_keyboard():
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
+            [KeyboardButton(text="📞 Зв'язатися зі мною", url="495607752")],
             [KeyboardButton(text="🔥 ЗНИЖКА НА МАСАЖ 20%!")],  # Верхня кнопка
             [KeyboardButton(text="Записатися на масаж")],
             [KeyboardButton(text="Опис масажів")],
