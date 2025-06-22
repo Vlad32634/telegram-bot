161043124aebecacdbfbd3a8d7fc8fa233fe526c
Vlad32634 <studentinfizan1@gmail.com>
2025-06-22 16:05:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -211,7 +211,8 @@ async def start_handler(message: types.Message):
         "Щоб користуватись ботом, підтвердіть, що ви не бот 👇",
         reply_markup=markup
     )
-   @router.callback_query(lambda c: c.data.startswith("verify:"))
+   
+@router.callback_query(lambda c: c.data.startswith("verify:"))
 async def process_verify(callback: types.CallbackQuery):
     user = callback.from_user
     user_id = user.id
